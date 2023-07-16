55533c254c7035cf9f7857d841edec5c81f9d359
Jon Olav Husabø Eikenes <jon.olav.eikenes@schibsted.com>
2023-07-16 14:39:04 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,168 @@
+<script>
+</script>
+
+<main>
+  <h1>På det høyeste vil GE Haliade-X rage dobbelt så høyt som Oslo Plaza</h1>
+
+  <div class="windmillcontainer">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      role="img"
+      viewBox="0 0 420 320"
+      class="s-XsEmFtvddWTw"
+    >
+      <title lang="en" class="s-XsEmFtvddWTw">Oslo Plaza and the Haliade-X windmill placed next to each other.</title>
+      <desc lang="en" class="s-XsEmFtvddWTw">Oslo Plaza is 117 meters tall, while the Haliade-X is 246 meters including the blades.</desc>
+      <style class="s-XsEmFtvddWTw">
+        .height-text {
+          font-family: Verdana;
+          font-size: 16px;
+          font-weight: bold;
+          text-anchor: middle;
+          fill: white;
+        }
+        .name-text {
+          font-family: Verdana;
+          font-size: 12px;
+          text-anchor: middle;
+          fill: white;
+        }
+        * {
+        }
+      </style>
+      <rect width="100%" height="100%" fill="#0089d1" class="s-XsEmFtvddWTw" />
+      <!-- Munchmuseet -->
+      <!-- 
+      <g transform="translate(-198,10)" class="s-XsEmFtvddWTw">
+        <path
+          fill="#fff"
+          d="M388.509 185.5h-26.541l7.932 23.389h.203v18.305h-22.779v17.084h3.727v1.222h45.006v-1.222h.588v-35.592h-.273l-7.863-23.186Z"
+          class="munch s-XsEmFtvddWTw"
+        />
+        <text class="height-text s-XsEmFtvddWTw" x="371.98" y="272">60 m</text>
+        <text class="name-text s-XsEmFtvddWTw" x="371.98" y="290"
+          >Munchmuseet</text
+        >
+        
+      </g>
+      -->
+      <!-- Oslo Plaza -->
+      <g transform="translate(-140,10)" class="s-XsEmFtvddWTw">
+        <path
+          fill="#fff"
+          d="M244.529 145.18h6.613V128.5l16.673 16.68V245.5h-23.286V145.18Z"
+          class="plaza s-XsEmFtvddWTw"
+        />
+        <text class="height-text s-XsEmFtvddWTw" x="256.17" y="272">117 m</text>
+        <text class="name-text s-XsEmFtvddWTw" x="256.17" y="290"
+          >Oslo Plaza</text
+        >
+      </g>
+      <!-- Haliade-X -->
+      <g class="windmill s-XsEmFtvddWTw" transform="translate(170,10)">
+        <path
+          fill="#DBDBDB"
+          d="M116.51 106.15h-9.43v9.99h9.43v-9.99Z"
+          class="s-XsEmFtvddWTw"
+        />
+        <path
+          fill="url(#gradient-b)"
+          d="M114.14 116.14C115.46 170.46 115.2 241 115.2 241l9.33 2.81v1.69H99.05v-1.69l9.33-2.81s-.26-70.54 1.06-124.86h4.7Z"
+          class="s-XsEmFtvddWTw"
+        />
+        <g class="blades s-XsEmFtvddWTw">
+          <path
+            fill="#fff"
+            d="M12.4 62.49c51.18 16.53 82.36 38.2 98.87 44.61l.52 4.05-2.77 2.95C87.63 96.55 57.38 78.53 12.33 63.03c0 0-.92-.33-.92-.54 0-.21.74-.08 1 0"
+            class="Vector s-XsEmFtvddWTw"
+          />
+          <path
+            fill="#fff"
+            d="M119.348 221.553c-11.274-52.588-8.097-90.426-10.801-107.929l3.247-2.475 3.94.924c-4.504 27.299-4.985 62.506 4.117 109.271 0 0 .174.962-.008 1.067-.181.105-.439-.601-.5-.866"
+            class="Vector s-XsEmFtvddWTw"
+          />
+          <path
+            fill="#fff"
+            d="M203.626 49.405c-39.905 36.058-74.262 52.226-88.068 63.319l-3.768-1.575-1.17-3.874c25.894-9.75 56.625-26.937 92.573-58.2 0 0 .746-.633.928-.528.182.105-.301.681-.5.866"
+            class="Vector s-XsEmFtvddWTw"
+          />
+        </g>
+        <path
+          fill="url(#gradient-c)"
+          d="M115.84 111.15c0 2.24-1.81 4.05-4.05 4.05-2.24 0-4.05-1.81-4.05-4.05 0-2.24 1.81-4.05 4.05-4.05a4.044 4.044 0 0 1 4.05 4.05Z"
+          class="s-XsEmFtvddWTw"
+        />
+        <text class="height-text s-XsEmFtvddWTw" x="111.79" y="272">246 m</text>
+        <text class="name-text s-XsEmFtvddWTw" x="111.79" y="290"
+          >GE Haliade-X</text
+        >
+      </g>
+      <defs class="s-XsEmFtvddWTw">
+        <radialGradient
+          id="gradient-c"
+          cx="50%"
+          cy="30%"
+          r="50%"
+          class="s-XsEmFtvddWTw"
+        >
+          <stop offset="0%" stop-color="#fff" class="s-XsEmFtvddWTw" />
+          <stop offset="100%" stop-color="#D9D9D9" class="s-XsEmFtvddWTw" />
+        </radialGradient>
+        <linearGradient
+          id="gradient-b"
+          x1="0"
+          y1="0"
+          x2="0"
+          y2="1"
+          gradientUnits="objectBoundingBox"
+          class="s-XsEmFtvddWTw"
+        >
+          <stop offset="0" stop-color="#E3E3E3" class="s-XsEmFtvddWTw" />
+          <stop offset="1" stop-color="#fff" class="s-XsEmFtvddWTw" />
+        </linearGradient>
+        <clipPath class="clippath-a s-XsEmFtvddWTw">
+          <path fill="#fff" d="M0 0h221v246H0z" class="s-XsEmFtvddWTw" />
+        </clipPath>
+      </defs>
+    </svg>
+  </div>
+</main>
+
+<style>
+  :global(body) {
+    background-color: #0089d1;
+  }
+
+  h1 {
+    margin: 40px 40px 40px 40px;
+    font-size: 2.5rem;
+    line-height: 1.2;
+    color: white;
+    text-align: left;
+  }
+
+  .windmillcontainer {
+    width: 100%;
+    max-height: 70vh;
+    padding-bottom: 75%; /* adjust to match SVG's aspect ratio */
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  .blades {
+    animation: rotate 10s linear infinite;
+    transform-origin: 111.79px 111.15px; /* adjust this to the center of your windmill */
+  }
+</style>
